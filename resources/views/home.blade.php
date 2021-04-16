9cf93cfbd0588cf26d9cd52c4fd616ad82224856
Slim Yelow <slimyelow@gmail.com>
2021-04-16 16:31:10 +0200

    add auth and management menu with kewl font awesome

---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="row text-center">
+
+                      <div class="col-sm-4">
+                        <a href="/management">
+                          <img width="130px" src="{{asset('images/management.svg')}}" alt="management">
+                          <h4>Management</h4>
+                        </a>
+                      </div>
+
+                      <div class="col-sm-4">
+                        <a href="/cashier">
+                          <img width="130px" src="{{asset('images/cashier.svg')}}" alt="cashier">
+                          <h4>Cashier</h4>
+                        </a>
+                      </div>
+
+                      <div class="col-sm-4">
+                        <a href="/report">
+                          <img width="130px" src="{{asset('images/report.svg')}}" alt="report">                          
+                          <h4>Report</h4>
+                        </a>
+                      </div>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
